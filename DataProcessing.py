bfdd8c56242b720d84e94e95b557e3319d9f9c2c
Mathias <mat.jos786@gmail.com>
2024-12-04 13:52:19 +0000

    testing .env access

--- DataProcessing.py
+++ DataProcessing.py
@@ -5,9 +5,6 @@ from dotenv import load_dotenv
 import os
 
 
-def configure():
-    load_dotenv()
-
 def GetHistorical():
     df = yf.download('^DJI', start='2013-01-01', end='2023-12-31')
 
@@ -18,14 +15,15 @@ def GetHistorical():
     df['Medium'] = (df['High'] + df['Low']) / 2
 
     #df.to_csv("HistoricalDataCSV.csv")
-    df.to_excel('HistoricalData.xlsx') # uses usd
+    #df.to_excel('HistoricalData.xlsx') # uses $USD
 
     return df.head()
 
 
 def GetEconomical():
-    os.getenv("fred_api_key")
+    print(os.getenv("FRED_API_KEY"))
 
 
-configure()
-GetHistorical()
+load_dotenv() #load env vars
+GetEconomical()
+#GetHistorical()
