4f4aaa6a01a13cf949bb83b7bb05888eef15898b
Mathias <mat.jos786@gmail.com>
2024-12-04 12:47:02 +0000

    Get Historical Data

---
+++ DataProcessing.py
@@ -0,0 +1,24 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+
+
+def GetHistorical():
+    df = yf.download('^DJI', start='2013-01-01', end='2023-12-31')
+
+    df.reset_index(inplace=True)
+    df["Date"] = df["Date"].dt.strftime('%Y-%m-%d')
+
+    # Creates new column in DF called medium
+    df['Medium'] = (df['High'] + df['Low']) / 2
+
+
+
+    #df.to_csv("HistoricalDataCSV.csv")
+    df.to_excel('HistoricalData.xlsx') # uses usd
+
+    return df.head()
+
+
+GetHistorical()
