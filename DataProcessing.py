6dc67cea0b49982deeb52b781ef8fad35b4528eb
Mathias <mat.jos786@gmail.com>
2024-12-04 13:19:46 +0000

    Added .env and FRED api

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,8 +1,12 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+from dotenv import load_dotenv
+import os
 
 
+def configure():
+    load_dotenv()
 
 def GetHistorical():
     df = yf.download('^DJI', start='2013-01-01', end='2023-12-31')
@@ -13,12 +17,15 @@ def GetHistorical():
     # Creates new column in DF called medium
     df['Medium'] = (df['High'] + df['Low']) / 2
 
-
-
     #df.to_csv("HistoricalDataCSV.csv")
     df.to_excel('HistoricalData.xlsx') # uses usd
 
     return df.head()
 
 
+def GetEconomical():
+    os.getenv("fred_api_key")
+
+
+configure()
 GetHistorical()
